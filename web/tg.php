835ebafcea7e95849bec379a64219e5726966d05
ledudu <saver.stone@gmail.com>
2020-08-02 10:44:01 +0800

    Create tg.php

---
+++ web/tg.php
@@ -0,0 +1,36 @@
+<?php
+$bot_api_key = 'CHANGE HERE';
+function send_get($urlstring){
+ $ch = curl_init();
+
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HEADER, false);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+ curl_setopt( $ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ curl_setopt($ch, CURLOPT_URL, $urlstring);
+ $result = curl_exec($ch);
+ //捕抓异常
+    if (curl_errno($ch)) {
+        $code = 400; //执行异常
+        $data = curl_error($ch);
+         curl_close($ch); 
+         return $data;
+    }
+ curl_close($ch); 
+ return $result;
+}
+$today = date('Y-m-d');
+$text = @$_GET["text"];
+$tgid = @$_GET["tgid"];
+$sendText = urlencode($today."\n".$text);
+if($text){
+ $url = "";
+ echo send_get($url);
+ echo $today;
+}else{
+ //echo "Please Input";
+ echo send_get('http://www.google.com');
+}
+?>
