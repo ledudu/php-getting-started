378be27edda4f08f467476df8dc7bb73fdce3e28
ledudu-office <saver.stone@gmail.com>
2021-02-10 14:26:57 +0800

    修改

--- web/hook.php
+++ web/hook.php
@@ -7,16 +7,31 @@
     $message = $update["message"]["text"];
     //$message = strtolower($message);
 
-    if (strpos($message, "/hello") === 0)
+    if (strpos($message, "/addredcode") === 0)
     {
-        $redcode = substr($message, 7);
+        $redcode = substr($message, strlen("/addredcode")+1);
         //$weather  = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=" . $location . "&appid=mytoken"), true)["weather"][0]["main"];
-        $sendmessage = "红包代码保存完毕";
+        $sendmessage = "整点红包代码保存完毕";
         file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=" . $sendmessage);
 
         if (strlen($redcode) != 0)
         {
             $myfile = fopen("redcode.txt", "w") or die("Unable to open file!");
+            fwrite($myfile, $redcode);
+            fclose($myfile);
+        }
+    }
+
+    if (strpos($message, "/addredcodehalf") === 0)
+    {
+        $redcode = substr($message, strlen("/addredcodehalf")+1);
+        //$weather  = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=" . $location . "&appid=mytoken"), true)["weather"][0]["main"];
+        $sendmessage = "超级红包代码保存完毕";
+        file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=" . $sendmessage);
+
+        if (strlen($redcode) != 0)
+        {
+            $myfile = fopen("redcodehalf.txt", "w") or die("Unable to open file!");
             fwrite($myfile, $redcode);
             fclose($myfile);
         }
